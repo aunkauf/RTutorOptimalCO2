f565891d2e787447ff22aae30793ba4f458c38c5
aunkauf <andreas.unkauf@uni-ulm.de>
2016-07-21 11:25:39 +0200

    Add files via upload

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorOptimalCO2"
+  
+  # Name of problem sets in the package
+  ps = c("RTutorOptimalCO2")
+  
+  list(package=package, ps = ps)
+}
